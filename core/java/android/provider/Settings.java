15b78481ec2fa10c64095c2fe4928c1e973500f6
Nico60 <ndhouailly@gmail.com>
2019-03-14 07:04:00 -0400

    NetworkTraffic: Show arrows in statusbar [1/2]
    arrow-edits:
    adapt code to our dynamic arrows method
    
    SKULSHADY: Reverse logic of the toggle
    spezi77: Adapt for statusbar traffic indicator
    
    Change-Id: Ie50dbe3f54495d4b5c0c0a51969df595de7a1386
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5397,6 +5397,13 @@ public final class Settings {
          * @hide
          */
         public static final String TORCH_POWER_BUTTON_GESTURE = "torch_power_button_gesture";
+        
+        
+        /**
+         * Whether to disable showing arrows in statusbar network traffic indicators
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_ARROW = "network_traffic_arrow";
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to
