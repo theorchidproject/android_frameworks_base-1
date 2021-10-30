87978a29f033ea9ae41d3a7008a843876f718cb0
neobuddy89 <neobuddy89@gmail.com>
2021-10-30 18:37:08 +0200

    SystemUI: Allow using 4G icon instead of LTE [1/2]
    Change-Id: I5dedd4317af66d6896b7803b228a53b59ea2c35a

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5301,6 +5301,13 @@ public final class Settings {
          */
         public static final String POWERMENU_EMERGENCY = "powermenu_emergency";
 
+
+        /**
+         * Whether to display 4G icon instead LTE
+         * @hide
+         */
+        public static final String SHOW_FOURG_ICON = "show_fourg_icon";
+
         /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
