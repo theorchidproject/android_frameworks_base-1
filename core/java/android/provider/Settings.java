89c8abf2def2156b8b6821cfb1c21a2518aa1e5a
orchidromdev <orchidromdev@gmail.com>
2022-11-10 21:41:42 +0000

    revert o0pl(Allow to quickly open Running Services from QS panel [1/2])

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5232,42 +5232,6 @@ public final class Settings {
          */
 
         /**
-         * Show Running Services icon in QS Footer.
-         * @hide
-         */
-        public static final String QS_FOOTER_SHOW_SERVICES = "qs_footer_show_services";
-
-          /**
-         * If On-The-Go should be displayed at the power menu.
-         *
-         * @hide
-         */
-        public static final String GLOBAL_ACTIONS_ONTHEGO = "global_actions_onthego";
-
-        /**
-         * The alpha value of the On-The-Go overlay.
-         *
-         * @hide
-         */
-        public static final String ON_THE_GO_ALPHA = "on_the_go_alpha";
-
-        /**
-         * Whether the service should restart itself or not.
-         *
-         * @hide
-         */
-        public static final String ON_THE_GO_SERVICE_RESTART = "on_the_go_service_restart";
-
-        /**
-         * The camera instance to use.
-         * 0 = Rear Camera
-         * 1 = Front Camera
-         *
-         * @hide
-         */
-        public static final String ON_THE_GO_CAMERA = "on_the_go_camera";
-
-        /**
          * How many rows to show in the qs panel when in portrait
          * @hide
          */
