5375b1432b10e0323da059616e92384d1fade026
orchidromdev <orchidromdev@gmail.com>
2022-11-15 06:52:28 +0000

    important delete duplicate  declaration
    Settings.java:5342: error: duplicate
    declaration of field: SHOW_FOURG_ICON
            public static final String SHOW_FOURG_ICON = "show_fourg_icon";

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5336,12 +5336,6 @@ public final class Settings {
 
 
         /**
-         * Whether to display 4G icon instead LTE
-         * @hide
-         */
-        public static final String SHOW_FOURG_ICON = "show_fourg_icon";
-
-        /**
          * Allow users to pull down the status bar quickly
          * @hide
          */
