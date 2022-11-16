94c56c038f8966ebf831daab9dc8fb44e7265544
orchidromdev <orchidromdev@gmail.com>
2022-11-16 07:36:43 +0000

    Revert "LockScreen: Add wake on plug [1/2]"
    This reverts commit 3238d83042c0215e0ac1321c962ea3a9ee90be64.

--- services/core/java/com/android/server/power/PowerManagerService.java
+++ services/core/java/com/android/server/power/PowerManagerService.java
@@ -1290,9 +1290,6 @@ public final class PowerManagerService extends SystemService
         resolver.registerContentObserver(Settings.Global.getUriFor(
                 Settings.Global.DEVICE_DEMO_MODE),
                 false, mSettingsObserver, UserHandle.USER_SYSTEM);
-        resolver.registerContentObserver(Settings.Global.getUriFor(
-            Settings.Global.WAKE_WHEN_PLUGGED_OR_UNPLUGGED),
-            false, mSettingsObserver, UserHandle.USER_ALL);       
 
         // Register for Lineage settings changes.
         resolver.registerContentObserver(LineageSettings.System.getUriFor(
@@ -1475,9 +1472,6 @@ public final class PowerManagerService extends SystemService
         mKeyboardBrightness = LineageSettings.Secure.getFloatForUser(resolver,
                 LineageSettings.Secure.KEYBOARD_BRIGHTNESS, mKeyboardBrightnessDefault,
                 UserHandle.USER_CURRENT);
-        mWakeUpWhenPluggedOrUnpluggedSetting = Settings.Global.getInt(resolver,
-                Settings.Global.WAKE_WHEN_PLUGGED_OR_UNPLUGGED,
-                (mWakeUpWhenPluggedOrUnpluggedConfig ? 1 : 0));
 
         mDirty |= DIRTY_SETTINGS;
     }
@@ -2406,7 +2400,7 @@ public final class PowerManagerService extends SystemService
     private boolean shouldWakeUpWhenPluggedOrUnpluggedLocked(
             boolean wasPowered, int oldPlugType, boolean dockedOnWirelessCharger) {
         // Don't wake when powered unless configured to do so.
-        if (mWakeUpWhenPluggedOrUnpluggedSetting == 0) {
+        if (!mWakeUpWhenPluggedOrUnpluggedSetting) {
             return false;
         }
 
