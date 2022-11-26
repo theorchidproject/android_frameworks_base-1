9c418f637934a50a49af215ba707c0ddf997b06c
orchidromdev <orchidromdev@gmail.com>
2022-11-26 08:54:14 +0000

    REVERT Powermenu: advanced reboot toggle - android 11
    REVERT Powermenu: advanced reboot toggle - android 11
    
    Update GlobalActionsDialogLite.java

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsImpl.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsImpl.java
@@ -86,7 +86,7 @@ public class GlobalActionsImpl implements GlobalActions, CommandQueue.Callbacks 
     }
 
     @Override
-    public void showShutdownUi(boolean isReboot, String reason, boolean advancedReboot) {
+    public void showShutdownUi(boolean isReboot, String reason, boolean rebootCustom) {
         ScrimDrawable background = new ScrimDrawable();
 
         final Dialog d = new Dialog(mContext,
@@ -150,8 +150,8 @@ public class GlobalActionsImpl implements GlobalActions, CommandQueue.Callbacks 
         reasonView.setTextColor(color);
         messageView.setTextColor(color);
 
-        messageView.setText(getRebootMessage(isReboot, reason, advancedReboot));
-        String rebootReasonMessage = getReasonMessage(reason, advancedReboot);
+        messageView.setText(getRebootMessage(isReboot, reason, rebootCustom));
+        String rebootReasonMessage = getReasonMessage(reason, rebootCustom);
         if (rebootReasonMessage != null) {
             reasonView.setVisibility(View.VISIBLE);
             reasonView.setText(rebootReasonMessage);
@@ -165,9 +165,9 @@ public class GlobalActionsImpl implements GlobalActions, CommandQueue.Callbacks 
         if (reason != null && reason.startsWith(PowerManager.REBOOT_RECOVERY_UPDATE)) {
             return R.string.reboot_to_update_reboot;
         } else if (reason != null && !custom && reason.equals(PowerManager.REBOOT_RECOVERY)) {
-            return R.string.reboot_to_recovery_message;
+            return com.android.systemui.R.string.global_action_restart_progress;
         } else if (reason != null && reason.equals(PowerManager.REBOOT_RECOVERY)) {
-            return com.android.internal.R.string.reboot_to_recovery_message;
+            return com.android.systemui.R.string.global_action_restart_recovery_progress;
         } else if (reason != null && reason.equals(PowerManager.REBOOT_BOOTLOADER)) {
             return com.android.systemui.R.string.global_action_restart_bootloader_progress;
         } else if (reason != null && reason.equals(PowerManager.REBOOT_DOWNLOAD)) {
