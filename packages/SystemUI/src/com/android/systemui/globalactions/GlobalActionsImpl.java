36e01ae847fd0adf3e6ab959ba748019575adb54
orchidromdev <orchidromdev@gmail.com>
2022-11-26 08:49:08 +0000

    REVERT Reboot Menu : add fastboot button

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsImpl.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsImpl.java
@@ -22,7 +22,6 @@ import android.annotation.StringRes;
 import android.app.Dialog;
 import android.content.Context;
 import android.os.PowerManager;
-import android.os.SystemProperties;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
@@ -170,13 +169,11 @@ public class GlobalActionsImpl implements GlobalActions, CommandQueue.Callbacks 
         } else if (reason != null && reason.equals(PowerManager.REBOOT_RECOVERY)) {
             return com.android.internal.R.string.reboot_to_recovery_message;
         } else if (reason != null && reason.equals(PowerManager.REBOOT_BOOTLOADER)) {
-            return R.string.reboot_to_bootloader_message;
+            return com.android.systemui.R.string.global_action_restart_bootloader_progress;
+        } else if (reason != null && reason.equals(PowerManager.REBOOT_DOWNLOAD)) {
+            return com.android.systemui.R.string.global_action_restart_download_progress;
         } else if (reason != null && reason.equals(PowerManager.REBOOT_FASTBOOT)) {
-            if (SystemProperties.getBoolean("ro.fastbootd.available", false)) {
-                return R.string.reboot_to_fastboot_message;
-            } else {
-                return R.string.reboot_to_fastbootd_message;
-            }
+            return com.android.systemui.R.string.global_action_restart_fastboot_progress;
         } else if (isReboot) {
             return com.android.systemui.R.string.global_action_restart_progress;
         } else {
